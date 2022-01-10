bd3df688a2946db3d0669215e4e9eec6f3fc5214
Grogl <ziga2003.groselj@gmail.com>
2022-01-10 11:04:17 +0100

    nadaljevanje vaje

--- NARC/vaja1.py
+++ NARC/vaja1.py
@@ -1,6 +1,20 @@
-a = 3
-b = 4
-c = (a**2 + b**2)**0.5
-print(c)
-print("hipotenuza =",c)
-print("hipotenuza =",c,"cm")
+#idRacuna = 1
+#ImeKlienta = "Satoshi Nakamoto"
+#Valuta = "Bitcoin"
+#StanjeVKonjusnici = 2911.77
+#veljavnost = "True"
+#print(idRacuna,ImeKlienta,Valuta,StanjeVKonjusnici,veljavnost)
+
+#a = 3
+#b = 4
+#c = (a**2 + b**2)**0.5
+#print(c)
+#print("hipotenuza =",c)
+#print("hipotenuza =",c,"cm")
+
+#st_ovac = 345
+#st_ovac += 1
+#print(st_ovac)
+
+cifra = int(input("Vnesi stevilo: "))
+print("Kvadrat stevvila ",cifra, " = ",cifra**2)
